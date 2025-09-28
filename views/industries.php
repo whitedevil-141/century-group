dcca993690290344c01f1a022d960e5603353c92
whitedevil-141 <white.devil.dev.141@gmail.com>
2025-09-28 11:40:37 +0600

    Initialized Industries page

--- views/industries.php
+++ views/industries.php
@@ -5,16 +5,184 @@
 
 <body class="">
     <?php require __DIR__ . '/layouts/navbar.php'; ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
+    <!--==============================
+    Breadcumb
+============================== -->
+    <div class="space">
+            <div class="container">
                 <div class="title-area text-center">
-                    <h2 class="sec-title">Industries</h2>
-                    <p class="sec-text">Century Group is a leading Bangladeshi conglomerate with ventures in real estate, hospitality, airways, finance, textiles, and more.</p>
+                    <span class="sub-title">What We Do?</span>
+                    <h2 class="sec-title text-theme">Our Industries</h2>
+                    <p class="sec-text">Since 1998, CENTRUY GROUP has been building a legacy of trust and innovation in Bangladesh.
                 </div>
             </div>
         </div>
-    </div>
+    <!--==============================
+    Service Area  
+    ==============================-->
+    <section class="service-area-2 space bg-gray">
+        <div class="sec-bg-shape2-1 spin shape-mockup d-xl-block d-none" data-bottom="5%" data-left="12%">
+            <img src="assets/img/shape/section_shape_2_1.jpg" alt="img">
+        </div>
+        <div class="sec-bg-shape2-2 wave-anim shape-mockup d-xl-block d-none" data-top="-3%" data-left="5%" data-bg-src="assets/img/shape/section_shape_2_2.jpg"></div>
+        <div class="sec-bg-shape2-3 jump shape-mockup d-xl-block d-none" data-top="10%" data-right="2%">
+            <img src="assets/img/shape/section_shape_2_3.jpg" alt="img">
+        </div>
+        <div class="container">
+            <div class="swiper th-slider" data-slider-options='{"breakpoints":{"0":{"slidesPerView":1},"576":{"slidesPerView":"1"},"768":{"slidesPerView":"2"},"992":{"slidesPerView":"2"},"1200":{"slidesPerView":"3"}}}'>
+                <div class="swiper-wrapper">
+
+                    <!-- Real Estate -->
+                    <div class="swiper-slide">
+                        <div class="service-card style2">
+                            <div class="service-card-icon">
+                                <img src="assets/img/icon/service-icon2-1.svg" alt="Icon">
+                            </div>
+                            <h3 class="box-title"><a href="real-estate.html">Real Estate & Housing</a></h3>
+                            <p class="box-text">Modern townships, apartments, and commercial spaces built with quality and innovation to shape better living in Bangladesh.</p>
+                            <div class="service-img img-shine">
+                                <img src="assets/img/service/1-2.png" alt="img">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Hotels & Resorts -->
+                    <div class="swiper-slide">
+                        <div class="service-card style2">
+                            <div class="service-card-icon">
+                                <img src="assets/img/icon/service-icon2-2.svg" alt="Icon">
+                            </div>
+                            <h3 class="box-title"><a href="hotel.html">Hotels & Resorts</a></h3>
+                            <p class="box-text">Premium resorts and luxury hotels in Kuakata and beyond, offering world-class hospitality experiences. experiences.</p>
+                            <div class="service-img img-shine">
+                                <img src="assets/img/service/1-2.png" alt="img">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Aviation -->
+                    <div class="swiper-slide">
+                        <div class="service-card style2">
+                            <div class="service-card-icon">
+                                <img src="assets/img/icon/plane-svgrepo-com.svg" alt="Icon">
+                            </div>
+                            <h3 class="box-title"><a href="aviation.html">Aviation & Cargo</a></h3>
+                            <p class="box-text">Century Airways provides air travel and cargo transport services, ensuring safe and efficient journeys.</p>
+                            <div class="service-img img-shine">
+                                <img src="assets/img/service/1-2.png" alt="img">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="container">
+            <div class="swiper th-slider" data-slider-options='{"breakpoints":{"0":{"slidesPerView":1},"576":{"slidesPerView":"1"},"768":{"slidesPerView":"2"},"992":{"slidesPerView":"2"},"1200":{"slidesPerView":"3"}}}'>
+                <div class="swiper-wrapper">
+
+                    <!-- Food & Beverages -->
+                    <div class="swiper-slide">
+                        <div class="service-card style2">
+                            <div class="service-card-icon">
+                                <img src="assets/img/icon/food-1-svgrepo-com.svg" alt="Icon">
+                            </div>
+                            <h3 class="box-title"><a href="food.html">Food & Beverages</a></h3>
+                            <p class="box-text">Century Farms produces fresh, safe, and high-quality food products, sweets, and bakery items for families nationwide.</p>
+                            <div class="service-img img-shine">
+                                <img src="assets/img/service/1-2.png" alt="img">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Garments & Textile -->
+                    <div class="swiper-slide">
+                        <div class="service-card style2">
+                            <div class="service-card-icon">
+                                <img src="assets/img/icon/shirt-svgrepo-com.svg" alt="Icon">
+                            </div>
+                            <h3 class="box-title"><a href="garments.html">Garments & Textile</a></h3>
+                            <p class="box-text">Sadia Garments Ltd. delivers export-quality apparel, strengthening Bangladesh’s global garment industry presence.</p>
+                            <div class="service-img img-shine">
+                                <img src="assets/img/service/1-2.png" alt="img">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Finance & Investment -->
+                    <div class="swiper-slide">
+                        <div class="service-card style2">
+                            <div class="service-card-icon">
+                                <img src="assets/img/icon/coin-svgrepo-com.svg" alt="Icon">
+                            </div>
+                            <h3 class="box-title"><a href="finance.html">Finance & Investment</a></h3>
+                            <p class="box-text">Century Leasing & Investment Co. Ltd. provides financial solutions and sustainable investment opportunities.</p>
+                            <div class="service-img img-shine">
+                                <img src="assets/img/service/1-2.png" alt="img">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="container">
+            <div class="swiper th-slider" data-slider-options='{"breakpoints":{"0":{"slidesPerView":1},"576":{"slidesPerView":"1"},"768":{"slidesPerView":"2"},"992":{"slidesPerView":"2"},"1200":{"slidesPerView":"3"}}}'>
+                <div class="swiper-wrapper">
+
+                    <!-- Travel & Tourism -->
+                    <div class="swiper-slide">
+                        <div class="service-card style2">
+                            <div class="service-card-icon">
+                                <img src="assets/img/icon/travel-svgrepo-com.svg" alt="Icon">
+                            </div>
+                            <h3 class="box-title"><a href="tourism.html">Travel & Tourism</a></h3>
+                            <p class="box-text">Century Travel Services provides travel, ticketing, and tour solutions for individuals and businesses worldwide.</p>
+                            <div class="service-img img-shine">
+                                <img src="assets/img/service/1-2.png" alt="img">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Parks & Recreation -->
+                    <div class="swiper-slide">
+                        <div class="service-card style2">
+                            <div class="service-card-icon">
+                                <img src="assets/img/icon/park-svgrepo-com.svg" alt="Icon">
+                            </div>
+                            <h3 class="box-title"><a href="recreation.html">Parks & Recreation</a></h3>
+                            <p class="box-text">We develop modern recreational spaces and theme parks that promote family entertainment and community bonding.</p>
+                            <div class="service-img img-shine">
+                                <img src="assets/img/service/1-2.png" alt="img">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Community Development -->
+                    <div class="swiper-slide">
+                        <div class="service-card style2">
+                            <div class="service-card-icon">
+                                <img src="assets/img/icon/people-group-svgrepo-com.svg" alt="Icon">
+                            </div>
+                            <h3 class="box-title"><a href="community.html">Community Development</a></h3>
+                            <p class="box-text">Through education, health, and social projects, we contribute to sustainable community growth and wellbeing.</p>
+                            <div class="service-img img-shine">
+                                <img src="assets/img/service/1-2.png" alt="img">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
     <!--==============================
 	Footer Area
 ==============================-->
